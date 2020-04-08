be656c6946a90008ef45b206bf1f762de4ba9a6a
Nathaly Cuya <nathalycuya@utexas.edu>
2020-04-08 01:32:21 -0500

    update-test

--- test/folders-endpoints.spec.js
+++ test/folders-endpoints.spec.js
@@ -8,7 +8,7 @@ describe('Folders Endpoints', function() {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DATABASE_URL,
+      connection: process.env.TEST_DB_URL,
     })
     app.set('db', db)
   })
--- test/notes-endpoints.spec.js
+++ test/notes-endpoints.spec.js
@@ -20,7 +20,7 @@ describe.only('Notes Endpoints', function() {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DATABASE_URL
+      connection: process.env.TEST_DB_URL
     })
     app.set('db', db)
   })
