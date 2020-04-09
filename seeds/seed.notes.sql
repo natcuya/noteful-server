7f356116ae876fb4c456d7a2fecf7aa3e0431809
Nathaly Cuya <nathalycuya@utexas.edu>
2020-04-09 06:09:27 -0500

    deploy read

--- seeds/seed.notes.sql
+++ seeds/seed.notes.sql
@@ -1,4 +1,4 @@
-INSERT INTO notes ( name, content, folder, date_modified)
+INSERT INTO notes ( title, content, folder_id, modified)
 VALUES
 ( 'Dogs', 'Content1', 1, '2019-01-03T00:00:00.000Z'),
 ( 'Cats', 'Content2', 2, '2019-01-03T00:00:00.000Z'),
